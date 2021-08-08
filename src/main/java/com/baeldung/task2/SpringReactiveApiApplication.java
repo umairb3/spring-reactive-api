4ddaa0269549383892a6ca36bdc56819e0bad94c
Umair Baig <umair.baig@ibm.com>
2021-08-08 22:54:44 +0500

    Initial commit

---
+++ src/main/java/com/baeldung/task2/SpringReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveApiApplication.class, args);
+	}
+
+}
