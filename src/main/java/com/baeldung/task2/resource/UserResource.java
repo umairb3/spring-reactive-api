00c937ae01a622776da5729fdfc4b2698c55e910
Umairb3 <umairb3@gmail.com>
2021-08-09 17:14:13 +0500

    resource emit after every second

--- src/main/java/com/baeldung/task2/resource/UserResource.java
+++ src/main/java/com/baeldung/task2/resource/UserResource.java
@@ -1,5 +1,7 @@
 package com.baeldung.task2.resource;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,8 @@ public class UserResource {
 	private UserService userService;
 
 	@GetMapping("/users")
-	public Flux<UserDto> getAllUsers() {
+	public Flux<UserDto> getAllUsers() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
 		return userService.findAllUsers();
 
 	}
